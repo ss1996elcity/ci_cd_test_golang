9001ee8f57b10bbedec8919515ab1f2c14127c36
Sergey SS <Dor7la96@mail.ru>
2022-02-08 18:06:39 +0300

    second_commit

--- tests_main.go
+++ tests_main.go
@@ -7,7 +7,7 @@ import (
 func TestPrintGreet(t *testing.T) {
 	var name string = "Alex"
 	testName := PrintGreet(name)
-	if testName != name {
+	if testName != "Hello, "+ name {
 		t.Errorf("Expected %s, received %v", name, testName)
 	}
 }
